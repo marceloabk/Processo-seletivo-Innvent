96b0eb426551fec729c95fe53ff2b55d12fce4a0
Marcelo Cristiano <marceloabk@hotmail.com>
2016-11-26 19:03:28 -0200

    creating a function to call API and calling at buttom click

--- innvent/app/assets/javascripts/currencylayer.js
+++ innvent/app/assets/javascripts/currencylayer.js
@@ -4,21 +4,29 @@ $(document).ready(function() {
     endpoint = 'live'
     access_key = '0a27d12beb3002410379fbfde65fe054';
 
-    $.ajax({
-        url: 'http://apilayer.net/api/' + endpoint + '?access_key=' + access_key,   
-        dataType: 'jsonp',
-        success: function(json) {
+    function apiRequest() {
+        // get the most recent exchange rates via the "live" endpoint:
+        $.ajax({
+            url: 'http://apilayer.net/api/' + endpoint + '?access_key=' + access_key,   
+            dataType: 'jsonp',
+            success: function(json) {
 
-            // exchange rata data is stored in json.quotes
-            alert(json.quotes.USDGBP);
-            
-            // source currency is stored in json.source
-            alert(json.source);
-            
-            // timestamp can be accessed in json.timestamp
-            alert(json.timestamp);
-            
-        }
+                // exchange rata data is stored in json.quotes
+                alert(json.quotes.USDGBP);
+                
+                // source currency is stored in json.source
+                alert(json.source);
+                
+                // timestamp can be accessed in json.timestamp
+                alert(json.timestamp);
+                
+            }
+        });
+    }
+
+
+    $("#first-slider-item").click(function() {
+        apiRequest();
     });
 
 });
