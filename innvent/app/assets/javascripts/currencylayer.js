ce968c65bc9d37530419481a83e48dad34d76e39
Marcelo Cristiano <marceloabk@hotmail.com>
2016-11-26 19:44:01 -0200

    creating last 7 days arrays

--- innvent/app/assets/javascripts/currencylayer.js
+++ innvent/app/assets/javascripts/currencylayer.js
@@ -4,31 +4,49 @@ $(document).ready(function() {
     endpoint = 'historical'
     access_key = '0a27d12beb3002410379fbfde65fe054';
 
+    USDBRL = new Array();
+    USDEUR = new Array();
+    USDARS = new Array();
+
     function apiRequest(date) {
         // get the most recent exchange rates via the "live" endpoint:
+
         $.ajax({
             url: 'http://apilayer.net/api/' + endpoint + '?access_key=' + 
             access_key + '&date=' + date + "&currencies=BRL,EUR,ARS",   
             dataType: 'jsonp',
             success: function(json) {
 
-                alert(JSON.stringify(json.quotes));
+                USDBRL.push(json.quotes.USDBRL);
+                USDEUR.push(json.quotes.USDEUR);
+                USDARS.push(json.quotes.USDARS);
 
             }
         });
+
     }
 
     function parseJSDateToYYYYMMDD(date) {
+
         var stringDate = date.toISOString().slice(0,10);
         return stringDate;
     }
 
+    function createHistoricalArrays() {
+
+        let date = new Date();
+
+
+        for (var i = 0; i < 7; i++) {
+            let formatedDate = parseJSDateToYYYYMMDD(date);
+            let conversion = apiRequest(formatedDate);
+            date.setDate(date.getDate() - 1);
+        }
+    }
+
 
     $("#first-slider-item").click(function() {
-        let date = new Date();
-        let formatedDate = parseJSDateToYYYYMMDD(date)
-        
-        apiRequest(formatedDate);
+        createHistoricalArrays();
     });
 
 });
