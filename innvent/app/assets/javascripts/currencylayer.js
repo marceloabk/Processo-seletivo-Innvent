38023c3bda9c5cecda8b9ad9367512f0d0684826
Marcelo Cristiano <marceloabk@hotmail.com>
2016-11-26 21:45:45 -0200

    refactoring currencylayer.js; comment all methods, change variables and methods names, and remove or commenting some magic numbers

--- innvent/app/assets/javascripts/currencylayer.js
+++ innvent/app/assets/javascripts/currencylayer.js
@@ -1,67 +1,95 @@
 $(document).ready(function() {
 
-    // set endpoint and your access key
+    // set endpoint and access key
     endpoint = 'historical'
     access_key = '0a27d12beb3002410379fbfde65fe054';
 
-    USDBRL = new Array();
-    USDEUR = new Array();
-    USDARS = new Array();
+    // currency arrays
+    usdbrl = new Array();
+    usdeur = new Array();
+    usdars = new Array();
 
+    const DAYS_OF_WEEK = 7;
+
+    // create a request at currency layer
     function apiRequest(date) {
-        // get the most recent exchange rates via the "live" endpoint:
 
+        // ajax request for historical endpoint
+        // returns historical rate of a given data of USD to BRL, USD to EUR and USD to ARS
         $.ajax({
             url: 'http://apilayer.net/api/' + endpoint + '?access_key=' + 
             access_key + '&date=' + date + "&currencies=BRL,EUR,ARS",   
             dataType: 'jsonp',
             success: function(json) {
 
-                USDBRL.push(json.quotes.USDBRL);
-                USDEUR.push(json.quotes.USDEUR);
-                USDARS.push(json.quotes.USDARS);
+                usdbrl.push(json.quotes.USDBRL);
+                usdeur.push(json.quotes.USDEUR);
+                usdars.push(json.quotes.USDARS);                
 
             },                   
             complete: function(){ 
+                
+                // compare arrays length with 7 to see if is the last apiRequest call
+                // it's 7 because it show result of the last week
 
-                if (USDBRL.length == 7 && USDARS.length == 7 && USDARS.length == 7) {
-                    convertArraysToBRL(USDARS);
-                    convertArraysToBRL(USDEUR);
+                if (usdbrl.length == DAYS_OF_WEEK && usdars.length == DAYS_OF_WEEK && usdars.length == DAYS_OF_WEEK) {
+                    convertArraysToBRL(usdars);
+                    convertArraysToBRL(usdeur);
                 }
             }
         });
 
     }
 
+    // receive a js object Date() and transform it to string with format YYYY-MM-DD
+    // the currency layer APIS needs the date with this format
     function parseJSDateToYYYYMMDD(date) {
 
+        // slice(0,10) to get the first 10 characters (YYYY-MM-DD)
         var stringDate = date.toISOString().slice(0,10);
+        
         return stringDate;
     }
 
-    function createHistoricalArrays() {
+    // fill usdbrl, usdeur and usdars with data from currency layer
+    function fillHistoricalRatesArray() {
 
+        // get current date
         let date = new Date();
 
 
-        for (var i = 0; i < 7; i++) {
+        for (var i = 0; i < DAYS_OF_WEEK; i++) {
+
             let formatedDate = parseJSDateToYYYYMMDD(date);
-            let conversion = apiRequest(formatedDate);
+
+            // get data for the given date
+            apiRequest(formatedDate);
+
+            // set the current date to date - 1. Js do all the math with date
             date.setDate(date.getDate() - 1);
         }
     }
 
+
+    /* if 1 USD = 3 BRL and
+     *    1 USD = 15 ARS then
+     *
+     *    3 BRL = 15 ARS and
+     *    1 BRL = 0.2 ARS    (BRL/ARS)
+     *
+     *
+     * this method use this logic to divide USDBRL by USDXXX and get how much 1 XXX worth in BRL currency
+     */
     function convertArraysToBRL(array) {
 
         for (var i = 0; i < array.length; i++) {
-            array[i] = USDBRL[i] / array[i];
+
+            array[i] = usdbrl[i] / array[i];
         }
 
     }
 
-
-    $("#first-slider-item").click(function() {
-        createHistoricalArrays();
-    });
+    // call fillHistoricalRatesArray when the page is ready
+    fillHistoricalRatesArray();
 
 });
