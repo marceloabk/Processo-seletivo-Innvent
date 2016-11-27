957ab63680b28ffd2cf345f4de0f64fc0422da25
Marcelo Cristiano <marceloabk@hotmail.com>
2016-11-27 17:33:46 -0200

    creating the 3 charts needed

--- innvent/app/assets/javascripts/highcharts.js
+++ innvent/app/assets/javascripts/highcharts.js
@@ -1,255 +1,279 @@
 $(document).ready(function () {
+    
     setTimeout(function() {
-            Highcharts.setOptions({
+
+        // chart theme
+        Highcharts.setOptions({
             colors: ['#2b908f', '#90ee7e', '#f45b5b', '#7798BF', '#aaeeee', '#ff0066', '#eeaaee',
-          '#55BF3B', '#DF5353', '#7798BF', '#aaeeee'],
-           chart: {
-              backgroundColor: {
-                 linearGradient: { x1: 0, y1: 0, x2: 1, y2: 1 },
-                 stops: [
+            '#55BF3B', '#DF5353', '#7798BF', '#aaeeee'],
+            chart: {
+                backgroundColor: {
+                    linearGradient: { x1: 0, y1: 0, x2: 1, y2: 1 },
+                    stops: [
                     [0, '#2a2a2b'],
                     [1, '#3e3e40']
-                 ]
-              },
-              style: {
-                 fontFamily: '\'Unica One\', sans-serif'
-              },
-              plotBorderColor: '#606063'
-           },
-           title: {
-              style: {
-                 color: '#E0E0E3',
-                 textTransform: 'uppercase',
-                 fontSize: '20px'
-              }
-           },
-           subtitle: {
-              style: {
-                 color: '#E0E0E3',
-                 textTransform: 'uppercase'
-              }
-           },
-           xAxis: {
-              gridLineColor: '#707073',
-              labels: {
-                 style: {
-                    color: '#E0E0E3'
-                 }
-              },
-              lineColor: '#707073',
-              minorGridLineColor: '#505053',
-              tickColor: '#707073',
-              title: {
-                 style: {
-                    color: '#A0A0A3'
-
-                 }
-              }
-           },
-           yAxis: {
-              gridLineColor: '#707073',
-              labels: {
-                 style: {
-                    color: '#E0E0E3'
-                 }
-              },
-              lineColor: '#707073',
-              minorGridLineColor: '#505053',
-              tickColor: '#707073',
-              tickWidth: 1,
-              title: {
-                 style: {
-                    color: '#A0A0A3'
-                 }
-              }
-           },
-           tooltip: {
-              backgroundColor: 'rgba(0, 0, 0, 0.85)',
-              style: {
-                 color: '#F0F0F0'
-              }
-           },
-           plotOptions: {
-              series: {
-                 dataLabels: {
-                    color: '#B0B0B3'
-                 },
-                 marker: {
-                    lineColor: '#333'
-                 }
-              },
-              boxplot: {
-                 fillColor: '#505053'
-              },
-              candlestick: {
-                 lineColor: 'white'
-              },
-              errorbar: {
-                 color: 'white'
-              }
-           },
-           legend: {
-              itemStyle: {
-                 color: '#E0E0E3'
-              },
-              itemHoverStyle: {
-                 color: '#FFF'
-              },
-              itemHiddenStyle: {
-                 color: '#606063'
-              }
-           },
-           credits: {
-              style: {
-                 color: '#666'
-              }
-           },
-           labels: {
-              style: {
-                 color: '#707073'
-              }
-           },
-
-           drilldown: {
-              activeAxisLabelStyle: {
-                 color: '#F0F0F3'
-              },
-              activeDataLabelStyle: {
-                 color: '#F0F0F3'
-              }
-           },
-
-           navigation: {
-              buttonOptions: {
-                 symbolStroke: '#DDDDDD',
-                 theme: {
-                    fill: '#505053'
-                 }
-              }
-           },
-
-           // scroll charts
-           rangeSelector: {
-              buttonTheme: {
-                 fill: '#505053',
-                 stroke: '#000000',
-                 style: {
-                    color: '#CCC'
-                 },
-                 states: {
-                    hover: {
-                       fill: '#707073',
-                       stroke: '#000000',
-                       style: {
-                          color: 'white'
-                       }
-                    },
-                    select: {
-                       fill: '#000003',
-                       stroke: '#000000',
-                       style: {
-                          color: 'white'
-                       }
-                    }
-                 }
-              },
-              inputBoxBorderColor: '#505053',
-              inputStyle: {
-                 backgroundColor: '#333',
-                 color: 'silver'
-              },
-              labelStyle: {
-                 color: 'silver'
-              }
-           },
-
-           navigator: {
-              handles: {
-                 backgroundColor: '#666',
-                 borderColor: '#AAA'
-              },
-              outlineColor: '#CCC',
-              maskFill: 'rgba(255,255,255,0.1)',
-              series: {
-                 color: '#7798BF',
-                 lineColor: '#A6C7ED'
-              },
-              xAxis: {
-                 gridLineColor: '#505053'
-              }
-           },
-
-           scrollbar: {
-              barBackgroundColor: '#808083',
-              barBorderColor: '#808083',
-              buttonArrowColor: '#CCC',
-              buttonBackgroundColor: '#606063',
-              buttonBorderColor: '#606063',
-              rifleColor: '#FFF',
-              trackBackgroundColor: '#404043',
-              trackBorderColor: '#404043'
-           },
-
-           // special colors for some of the
-           legendBackgroundColor: 'rgba(0, 0, 0, 0.5)',
-           background2: '#505053',
-           dataLabelsColor: '#B0B0B3',
-           textColor: '#C0C0C0',
-           contrastTextColor: '#F0F0F3',
-           maskColor: 'rgba(255,255,255,0.3)'
-        });
-        
-        var chart1 = new Highcharts.Chart({
-            chart: {
-                renderTo: 'container',
-                zoomType: 'x',
+                    ]
+                },
+                style: {
+                    fontFamily: '\'Unica One\', sans-serif'
+                },
+                plotBorderColor: '#606063'
             },
             title: {
-                text: 'BRL to USD exchange rate over time'
+                style: {
+                    color: '#E0E0E3',
+                    textTransform: 'uppercase',
+                    fontSize: '20px'
+                }
             },
             subtitle: {
-                text: document.ontouchstart === undefined ?
-                        'Click and drag in the plot area to zoom in' : 'Pinch the chart to zoom in'
+                style: {
+                    color: '#E0E0E3',
+                    textTransform: 'uppercase'
+                }
             },
             xAxis: {
-                type: 'datetime'
+                gridLineColor: '#707073',
+                labels: {
+                    style: {
+                        color: '#E0E0E3'
+                    }
+                },
+                lineColor: '#707073',
+                minorGridLineColor: '#505053',
+                tickColor: '#707073',
+                title: {
+                    style: {
+                        color: '#A0A0A3'
+
+                    }
+                }
             },
             yAxis: {
+                gridLineColor: '#707073',
+                labels: {
+                    style: {
+                        color: '#E0E0E3'
+                    }
+                },
+                lineColor: '#707073',
+                minorGridLineColor: '#505053',
+                tickColor: '#707073',
+                tickWidth: 1,
                 title: {
-                    text: 'Exchange rate'
+                    style: {
+                        color: '#A0A0A3'
+                    }
+                }
+            },
+            tooltip: {
+                backgroundColor: 'rgba(0, 0, 0, 0.85)',
+                style: {
+                    color: '#F0F0F0'
                 }
             },
             plotOptions: {
-                area: {
-                    fillColor: {
-                        linearGradient: {
-                            x1: 0,
-                            y1: 0,
-                            x2: 0,
-                            y2: 1
-                        },
-                        stops: [
-                            [0, Highcharts.getOptions().colors[0]],
-                            [1, Highcharts.Color(Highcharts.getOptions().colors[0]).setOpacity(0).get('rgba')]
-                        ]
+                series: {
+                    dataLabels: {
+                        color: '#B0B0B3'
                     },
                     marker: {
-                        radius: 2
-                    },
-                    lineWidth: 1,
-                    states: {
-                        hover: {
-                            lineWidth: 1
-                        }
-                    },
-                    threshold: null
+                        lineColor: '#333'
+                    }
+                },
+                boxplot: {
+                    fillColor: '#505053'
+                },
+                candlestick: {
+                    lineColor: 'white'
+                },
+                errorbar: {
+                    color: 'white'
                 }
             },
-            series: [{
-                type: 'area',
-                name: 'USD to EUR',
-                data: usdbrl
-            }]
-        });
-    }, 3000);
+            legend: {
+                itemStyle: {
+                    color: '#E0E0E3'
+                },
+                itemHoverStyle: {
+                    color: '#FFF'
+                },
+                itemHiddenStyle: {
+                    color: '#606063'
+                }
+            },
+            credits: {
+                style: {
+                    color: '#666'
+                }
+            },
+            labels: {
+                style: {
+                    color: '#707073'
+                }
+            },
+
+            drilldown: {
+                activeAxisLabelStyle: {
+                    color: '#F0F0F3'
+                },
+                activeDataLabelStyle: {
+                    color: '#F0F0F3'
+                }
+            },
+
+            navigation: {
+                buttonOptions: {
+                    symbolStroke: '#DDDDDD',
+                    theme: {
+                        fill: '#505053'
+                    }
+                }
+            },
+
+            // scroll charts
+            rangeSelector: {
+                buttonTheme: {
+                    fill: '#505053',
+                    stroke: '#000000',
+                    style: {
+                        color: '#CCC'
+                    },
+                    states: {
+                        hover: {
+                            fill: '#707073',
+                            stroke: '#000000',
+                            style: {
+                                color: 'white'
+                            }
+                        },
+                        select: {
+                            fill: '#000003',
+                            stroke: '#000000',
+                            style: {
+                                color: 'white'
+                            }
+                        }
+                    }
+                },
+                inputBoxBorderColor: '#505053',
+                inputStyle: {
+                    backgroundColor: '#333',
+                    color: 'silver'
+                },
+                labelStyle: {
+                    color: 'silver'
+                }
+            },
+            navigator: {
+                handles: {
+                    backgroundColor: '#666',
+                    borderColor: '#AAA'
+                },
+                outlineColor: '#CCC',
+                maskFill: 'rgba(255,255,255,0.1)',
+                series: {
+                    color: '#7798BF',
+                    lineColor: '#A6C7ED'
+                },
+                xAxis: {
+                    gridLineColor: '#505053'
+                }
+            },
+            scrollbar: {
+                barBackgroundColor: '#808083',
+                barBorderColor: '#808083',
+                buttonArrowColor: '#CCC',
+                buttonBackgroundColor: '#606063',
+                buttonBorderColor: '#606063',
+                rifleColor: '#FFF',
+                trackBackgroundColor: '#404043',
+                trackBorderColor: '#404043'
+            },
+            // special colors for some of the
+            legendBackgroundColor: 'rgba(0, 0, 0, 0.5)',
+            background2: '#505053',
+            dataLabelsColor: '#B0B0B3',
+            textColor: '#C0C0C0',
+            contrastTextColor: '#F0F0F3',
+            maskColor: 'rgba(255,255,255,0.3)'
+            });
+
+        const chartData = [
+            [
+                "USD para BRL",
+                "Taxa de câmbio de USD para BRL ao longo dos últimos 7 dias",
+                "usd-container",
+                usdbrl
+            ],
+            [
+                "EUR para BRL",
+                "Taxa de câmbio de EUR para BRL ao longo dos últimos 7 dias",
+                "eur-container",
+                usdeur
+            ],
+            [
+                "ARS para BRL",
+                "Taxa de câmbio de ARS para BRL ao longo dos últimos 7 dias",
+                "ars-container",
+                usdars
+            ]
+        ];
+        // creating 3 charts
+        for (var i = 0; i < 3; i++) {
+            var chart = new Highcharts.Chart({
+                chart: {
+                    renderTo: chartData[i][2],
+                    zoomType: 'x',
+                },
+                title: {
+                    text: chartData[i][1]
+                },
+                subtitle: {
+                    text: document.ontouchstart === undefined ?
+                    'Clique e arraste na área de desenho para aumentar o zoom' : 
+                    'Clique no gráfico para aumentar o zoom'
+                },
+                xAxis: {
+                    type: 'datetime'
+                },
+                yAxis: {
+                    title: {
+                        text: 'Taxa de câmbio'
+                    }
+                },
+                plotOptions: {
+                    area: {
+                        fillColor: {
+                            linearGradient: {
+                                x1: 0,
+                                y1: 0,
+                                x2: 0,
+                                y2: 1
+                            },
+                            stops: [
+                            [0, Highcharts.getOptions().colors[0]],
+                            [1, Highcharts.Color(Highcharts.getOptions().colors[0]).setOpacity(0).get('rgba')]
+                            ]
+                        },
+                        marker: {
+                            radius: 2
+                        },
+                        lineWidth: 1,
+                        states: {
+                            hover: {
+                                lineWidth: 1
+                            }
+                        },
+                        threshold: null
+                    }
+                },
+                series: [{
+                    type: 'area',
+                    name: chartData[i][0],
+                    data: chartData[i][3]
+                }]
+            });
+        }
+    }, 2000);
 });
