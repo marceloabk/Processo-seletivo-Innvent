21574f833088cc7e3fcfbf89ce276ff6c5c513da
Marcelo Cristiano <marceloabk@hotmail.com>
2016-11-30 02:47:02 -0200

    removing duplicated code at test

--- spec/javascripts/currencylayer_spec.js
+++ spec/javascripts/currencylayer_spec.js
@@ -3,18 +3,15 @@
 
 describe("CurrencyLayer#parseJSDateToYYYYMMDD", function() {
 
-  it("parse a js Date to YYYY-MM-DD format", function() {
   	var date = new Date(2016,10,30);
   	var formattedDate = parseJSDateToYYYYMMDD(date);
 
-    expect(formattedDate).toBe("2016-11-30");
-  });
+	it("parse a js Date to YYYY-MM-DD format", function() {
+		expect(formattedDate).toBe("2016-11-30");
+	});
 
-  it("should not parse a js Date to YYYY/MM/DD format", function() {
-  	var date = new Date(2016,10,30);
-  	var formattedDate = parseJSDateToYYYYMMDD(date);
-
-    expect(formattedDate).not.toBe("2016/11/30");
-  });
+	it("should not parse a js Date to YYYY/MM/DD format", function() {
+		expect(formattedDate).not.toBe("2016/11/30");
+	});
 
 });
